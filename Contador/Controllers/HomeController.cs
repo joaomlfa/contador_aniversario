ac783ec2d525c297c7d6e84ea78b49cceec3909d
Joao Moreira <joaomlfa@hotmail.com>
2020-08-17 22:31:51 -0300

    Versão Final backup PC Base

--- Contador/Controllers/HomeController.cs
+++ Contador/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace Contador.Controllers
         {
             return View();
         }
+        public IActionResult Diario()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- Contador/wwwroot/js/site.js
+++ Contador/wwwroot/js/site.js
@@ -1,5 +1,25 @@
 ﻿$(document).ready(function () {
-    $("#clock").countdown("2021/06/30", function (event) {
-        $(this).html(event.strftime("%D Dias %H:%M:%S"));
-    });
+    const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+    let countDown = new Date('Jun 30, 2021 00:00:00').getTime(),
+        x = setInterval(function () {
+
+            let now = new Date().getTime(),
+                distance = countDown - now;
+
+            document.getElementById('days').innerText = Math.floor(distance / (day)),
+                document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+                document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+                document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+
+            //do something later when date is reached
+            //if (distance < 0) {
+            //  clearInterval(x);
+            //  'IT'S MY BIRTHDAY!;
+            //}
+
+        }, second)
 });
